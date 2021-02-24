690ae7903ea8f962fda5387b2801f1ff28d7b0d1
Emanuele Parrinello <parry84@gmail.com>
2021-02-24 23:01:45 +0100

    🎨 refactor startup script

--- elm/index.js
+++ elm/index.js
@@ -1,37 +1,37 @@
 import { Elm } from "./Main.elm";
 
-// Get all elm nodes
-const elmNodes = document.querySelectorAll(".elm");
-
-// Initialize Elm on each elmNode
-elmNodes.forEach((node) => {
-  const app = Elm.Main.init({
-    node,
-    flags: getFlags(node.dataset.flags),
-  });
-
-  app.ports.copyLink.subscribe(() => {
-    const copyText = document.querySelector('#link').textContent;
-    const textArea = document.createElement('textarea');
-    textArea.textContent = copyText;
-    document.body.append(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    textArea.remove();
-  });
-
-  app.ports.copySecret.subscribe(() => {
-    const copyText = document.querySelector('#secret').textContent;
-    const textArea = document.createElement('textarea');
-    textArea.textContent = copyText;
-    document.body.append(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    textArea.remove();
-  });
-});
-
-// Parse the JSON from IHP or return null if there is none
 function getFlags(data) {
   return data ? JSON.parse(data) : null;
 }
+
+function initializeWidgets() {
+  const elmNodes = document.querySelectorAll(".elm");
+  elmNodes.forEach((node) => {
+    const app = Elm.Main.init({
+      node,
+      flags: getFlags(node.dataset.flags),
+    });
+    
+    app.ports.copyLink.subscribe(copySubscription('#link'));
+    app.ports.copySecret.subscribe(copySubscription('#secret'));
+  });
+}
+
+const copySubscription = (selector) => () => {
+  const copyText = document.querySelector(selector).textContent;
+  const textArea = document.createElement('textarea');
+  textArea.textContent = copyText;
+  document.body.append(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  textArea.remove();
+}
+
+window.addEventListener("load", (event) => {
+  initializeWidgets();
+});
+
+// Initializes Elm on Turbolinks transition
+document.addEventListener("turbolinks:load", (e) => {
+  initializeWidgets();
+});
